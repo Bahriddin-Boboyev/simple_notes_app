0e368728c5e6221819dd3295e77e6fbc19321942
Bahriddin <113900960+Bahriddin-Boboyev@users.noreply.github.com>
2023-04-17 23:20:31 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "OPTIONS,DELETE,GET,PUT,POST,PATCH",
+          },
+          {
+            key: "Access-Control-Expose-Headers",
+            value: "X-Total-Count, Content-Range",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "Origin, X-Requested-With, Content-Type, Accept, Range, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+};
+
